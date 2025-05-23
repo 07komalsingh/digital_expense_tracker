8228b247b0bc5514de3a14ab160182452c5308dd
07komalsingh <100020466+07komalsingh@users.noreply.github.com>
2025-05-23 13:20:03 +0530

    Update index.js
    updated

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,6 +1,3 @@
-// controllers/index.js
-// Barrel export file to re-export all controllers
-
 export * from './authController.js';
 export * from './categoryController.js';
 export * from './transactionController.js';
